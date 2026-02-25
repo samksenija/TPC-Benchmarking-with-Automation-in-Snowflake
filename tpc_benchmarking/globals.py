e93196e43d55c1ed955078cd9d2061476fea2d2a
ksenijasam <ksenija2702@gmail.com>
2026-02-25 10:54:37 +0100

    added config

--- tpc_benchmarking/globals.py
+++ tpc_benchmarking/globals.py
@@ -1,12 +1,16 @@
 import snowflake.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 conn = snowflake.connector.connect(
-    user='user',
-    password='password',
-    account='your_account',
-    warehouse='your_warehouse',
-    database='your_database',
-    schema='your_schema'
+    user=os.getenv("user"),
+    password=os.getenv("password"),
+    account=os.getenv("account"),
+    warehouse=os.getenv("warehouse"),
+    database=os.getenv("database"),
+    schema=os.getenv("schema")
 )
 
 #this is global configuration of database & schema
